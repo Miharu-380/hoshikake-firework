e9122633297be54fcba65cedde29e2eca17cd734
Miharu-380 <38t.ajlkk@gmail.com>
2021-08-19 16:45:10 +0900

    Add cruise

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// header
 $(function () {
   var head = $('#header-scroll');
   head.hide();
@@ -10,3 +11,36 @@ $(function () {
       }
   });
 })
+
+//slide
+$(function(){
+  $('.slide-l').addClass('move');
+  $(window).scroll(function(){
+    $(".slide-l").each(function(){
+      var imgPos = $(this).offset().top;
+      var scroll = $(window).scrollTop();
+      var windowHeight = $(window).height();
+      if (scroll > imgPos - windowHeight + windowHeight/5){
+        $(this).removeClass('move');
+      } else {
+        $(this).addClass('move');
+      }
+    });
+  });
+});
+
+$(function(){
+  $('.slide-r').addClass('move');
+  $(window).scroll(function(){
+    $(".slide-r").each(function(){
+      var imgPos = $(this).offset().top;
+      var scroll = $(window).scrollTop();
+      var windowHeight = $(window).height();
+      if (scroll > imgPos - windowHeight + windowHeight/5){
+        $(this).removeClass('move');
+      } else {
+        $(this).addClass('move');
+      }
+    });
+  });
+});
