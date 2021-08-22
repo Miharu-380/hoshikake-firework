b5297bc159f9f6d66807e164a5c8d1025c46835d
Miharu-380 <38t.ajlkk@gmail.com>
2021-08-22 16:40:50 +0900

    Add responsive half

--- script.js
+++ script.js
@@ -60,3 +60,23 @@ $(function(){
     });
   });
 });
+
+// ハンバーガーメニュー
+$('.menu-trigger').on('click',function(){
+  if($(this).hasClass('active')){
+    $(this).removeClass('active');
+    $('nav').removeClass('open');
+    $('.overlay').removeClass('open');
+  } else {
+    $(this).addClass('active');
+    $('nav').addClass('open');
+    $('.overlay').addClass('open');
+  }
+});
+$('.overlay').on('click',function(){
+  if($(this).hasClass('open')){
+    $(this).removeClass('open');
+    $('.menu-trigger').removeClass('active');
+    $('nav').removeClass('open');
+  }
+});
